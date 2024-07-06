55f1aec2fc57bbe488bda39f9dab1da497ef60a3
artsi0m <karakin2000@gmail.com>
2024-07-06 23:17:19 +0300

    Do not add same mode to already present mode var in prop line

--- howm-use-any-markup.el
+++ howm-use-any-markup.el
@@ -5,31 +5,40 @@
 (defun howm-org-mode ()
   "Set org as major and howm as minor mode for a file.
    Add first with file local variable specifying org as major mode"
-    (interactive)
-  (org-mode)
+  (interactive)
   (howm-mode)
-  (add-file-local-variable-prop-line 'mode 'org))
+  (unless (eq major-mode 'org-mode)
+    (org-mode)
+    (howm-mode)
+  (add-file-local-variable-prop-line 'mode 'org)))
+
 
 (defun howm-markdown-mode ()
   "Set markdown as major and howm as minor mode for a file.
    Add first with file local variable specifying markdown as major mode"
   (interactive)
-  (markdown-mode)
   (howm-mode)
-  (add-file-local-variable-prop-line 'mode 'markdown))
+  (unless (eq major-mode 'markdown-mode)
+    (markdown-mode)
+    (howm-mode)
+  (add-file-local-variable-prop-line 'mode 'markdown)))
 
 (defun howm-rst-mode ()
   "Set rst as major and howm as minor mode for a file.
    Add first with file local variable specifying rst as major mode"
   (interactive)
-  (rst-mode)
   (howm-mode)
-  (add-file-local-variable-prop-line 'mode 'rst))
+    (unless (eq major-mode 'rst-mode)
+    (rst-mode)
+    (howm-mode)
+  (add-file-local-variable-prop-line 'mode 'rst)))
 
 (defun howm-adoc-mode ()
   "Set adoc as major and howm as minor mode for a file.
    Add first with file local variable specifying adoc as major mode"
   (interactive)
-  (adoc-mode)
   (howm-mode)
-  (add-file-local-variable-prop-line 'mode 'adoc))
+    (unless (eq major-mode 'adoc-mode)
+    (adoc-mode)
+    (howm-mode)
+  (add-file-local-variable-prop-line 'mode 'adoc)))
