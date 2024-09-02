603d5ed451ae6fa6d4a9a7840916ba3cd73bb41c
Artyom Karakin <karakin2000@gmail.com>
2024-09-02 17:43:17 +0300

    Function both in interactive and not interactive way

--- howm-use-any-markup.el
+++ howm-use-any-markup.el
@@ -4,11 +4,19 @@
 ;; Copyright (C) 2024  "Artsiom Karakin" karakin2000@gmail.com
 ;; See LICENSE.txt for license of this code
 
-(defun howm-insert-prop-line ()
-"Activate major mode and modify the file so that this mode is activated
-automatically the next time it is opened"
-    (interactive)
-    (howm-mode)
+(defun howm-insert-prop-line (mode)
+"Enable & save major mode to prop-line to use with howm in the file local variable"
+(howm-mode)
+(let* ((modes (mapcar #'cdr auto-mode-alist))
+       (mode-name (symbol-name mode))
+       (mode (intern-soft mode-name)))
+  (unless (or (null mode)
+	      (eq mode major-mode)))
+  (funcall mode)
+  (howm-mode)
+  (add-file-local-variable-prop-line
+   'mode (intern (string-trim-right mode-name "-mode\\'"))))
+    (interactive
     (let*
 	((modes (mapcar #'cdr auto-mode-alist))
 	 (mode-name (completing-read "Choose major mode: " modes))
@@ -18,5 +26,5 @@ automatically the next time it is opened"
 	(funcall mode)
 	(howm-mode)
 	(add-file-local-variable-prop-line
-	 'mode (intern (string-trim-right mode-name "-mode\\'"))))))
+	 'mode (intern (string-trim-right mode-name "-mode\\'")))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
