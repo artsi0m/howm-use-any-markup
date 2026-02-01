76993a22895af1cb615a286041accbbec7c3af2a
Корякин Артём <karakin2000@gmail.com>
2026-02-01 21:30:44 +0300

    - code duplication. Thanks to Ilya Chernyshov for the refactoring
    https://github.com/ichernyshovvv

--- howm-use-any-markup.el
+++ howm-use-any-markup.el
@@ -34,28 +34,15 @@
 
 (require 'howm)
 
-
 (defun howm-insert-prop-line (&optional mode)
-"Enable & save major mode to prop-line to use with howm in the file local variable"
-(howm-mode)
-(let* ((modes (mapcar #'cdr auto-mode-alist))
-       (mode-name (symbol-name mode))
-       (mode (intern-soft mode-name)))
-  (unless (or (null mode)
-	      (eq mode major-mode)))
+  "Enable & save major mode to prop-line to use with howm in the file
+  local variable"
+  (interactive (list (intern
+                      (completing-read "Choose major mode: "
+                                       (mapcar #'cdr auto-mode-alist)
+                                       nil t))))
   (funcall mode)
   (howm-mode)
   (add-file-local-variable-prop-line
-   'mode (intern (string-trim-right mode-name "-mode\\'"))))
-    (interactive
-    (let*
-	((modes (mapcar #'cdr auto-mode-alist))
-	 (mode-name (completing-read "Choose major mode: " modes))
-	 (mode (intern-soft mode-name)))
-      (unless (or (null mode)
-	  (eq mode major-mode))
-	(funcall mode)
-	(howm-mode)
-	(add-file-local-variable-prop-line
-	 'mode (intern (string-trim-right mode-name "-mode\\'")))))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   'mode (intern (string-trim-right (symbol-name mode) "-mode"))))
+;; programmatically:  (howm-insert-prop-line 'c-mode)
